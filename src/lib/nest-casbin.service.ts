9136ec011d617a9615539dd1ffa1bfd1fd8d382f
Rex Isaac Raphael <rex.raphael@outlook.com>
2019-12-01 09:48:01 +0100

    update casbin

--- src/lib/nest-casbin.service.ts
+++ src/lib/nest-casbin.service.ts
@@ -24,6 +24,86 @@ export class NestCasbinService {
     }
   }
 
+  public async getPolicy(): Promise<string[][]> {
+    return this.enforcer.getPolicy();
+  }
+
+  public async getAllRoles(): Promise<string[]> {
+    return this.enforcer.getAllRoles();
+  }
+
+  public async getAllObjects(): Promise<string[]> {
+    return this.enforcer.getAllObjects();
+  }
+
+  public async getAllSubjects(): Promise<string[]> {
+    return this.enforcer.getAllSubjects();
+  }
+
+  public async getUsersForRole(name: string, domain?: string): Promise<string[]> {
+    return this.enforcer.getUsersForRole(name, domain);
+  }
+
+  public async hasRoleForUser(user: string, role: string, domain?: string): Promise<boolean> {
+    return this.enforcer.hasRoleForUser(user, role, domain);
+  }
+
+  public async addRoleForUser(user: string, role: string, domain?: string): Promise<boolean> {
+    return this.enforcer.addRoleForUser(user, role, domain);
+  }
+
+  public async deleteRoleForUser(user: string, role: string, domain?: string): Promise<boolean> {
+    return this.enforcer.deleteRoleForUser(user, role, domain);
+  }
+
+  public async deleteRolesForUser(user: string, domain?: string): Promise<boolean> {
+    return this.enforcer.deleteRolesForUser(user, domain);
+  }
+
+  public async deleteUser(user: string): Promise<boolean> {
+    return this.enforcer.deleteUser(user);
+  }
+
+  public async deleteRole(role: string): Promise<boolean> {
+    return this.enforcer.deleteRole(role);
+  }
+
+  public async deletePermission(...permissions: string[]): Promise<boolean> {
+    return this.enforcer.deletePermission(...permissions);
+  }
+
+  public async addPermissionForUser(user: string,...permissions: string[]): Promise<boolean> {
+    return this.enforcer.addPermissionForUser(user, ...permissions);
+  }
+
+  public async deletePermissionForUser(user: string,...permissions: string[]): Promise<boolean> {
+    return this.enforcer.deletePermissionForUser(user, ...permissions);
+  }
+
+  public async deletePermissionsForUser(user: string): Promise<boolean> {
+    return this.enforcer.deletePermissionsForUser(user);
+  }
+
+  public async getPermissionsForUser(user: string): Promise<string[][]> {
+    return this.enforcer.getPermissionsForUser(user);
+  }
+
+  public async hasPermissionForUser(user: string, ...permissions: string[]): Promise<boolean> {
+    return await this.enforcer.hasPermissionForUser(user, ...permissions);
+  }
+
+  public async getAllActions(): Promise<string[]> {
+    return this.enforcer.getAllActions();
+  }
+
+  public async hasPolicy(...params: string[]): Promise<boolean> {
+    return await this.enforcer.hasPolicy(...params);
+  }
+
+  public async hasNamedPolicy(p: string, ...params: string[]): Promise<boolean> {
+    return await this.enforcer.hasNamedPolicy(p, ...params);
+  }
+
   // TODO: edit this in adapter to make it query from database
   // the operation will look like `await this.enforcer.getAdapter().enforce()`
   public async checkPermission(...params: any[]) {
